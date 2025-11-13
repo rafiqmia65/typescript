976ef7cdb3a7064fd01c65d0ab70fd1b022fccb5
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-11-13 15:30:46 +0600

    first commit

---
+++ dist/primitive.js
@@ -0,0 +1,14 @@
+"use strict";
+// JS-Ts
+// string, number, boolean, undefined and null
+Object.defineProperty(exports, "__esModule", { value: true });
+// Ts => never, unknown and void
+let userName = "Rafiq";
+console.log(userName);
+let userId = 833;
+console.log(userId);
+let isAdmin = true;
+let a = undefined;
+let b = null;
+console.log(b);
+//# sourceMappingURL=primitive.js.map
---
+++ src/primitive.ts
@@ -0,0 +1,19 @@
+// JS-Ts
+// string, number, boolean, undefined and null
+
+// Ts => never, unknown and void
+
+let userName: string= "Rafiq";
+
+console.log(userName);
+
+let userId: number=833;
+
+console.log(userId);
+
+let isAdmin: boolean=true;
+
+let a: undefined=undefined;
+
+let b: null=null;
+console.log(b);
