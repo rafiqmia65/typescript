e4af35b78ff27d04aa681842ea00d9a70fd844db
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-11-14 21:40:08 +0600

    Non Primtive Data

--- dist/nonPrimitive.js
+++ dist/nonPrimitive.js
@@ -1,3 +1,22 @@
 "use strict";
+// array , object
 Object.defineProperty(exports, "__esModule", { value: true });
+// ts - tuple
+let bazarList = ["eggs", "milk", "sugar"];
+bazarList.push(true);
+let mixedArr = ["eggs", 12, "milk", 1, "sugar", 2];
+mixedArr.push(true);
+let coordinates = [20, 30, 50];
+let couple = ["Husband", "wife"];
+let destination = ["Dhaka", "Chattogram", 3];
+// reference type : object
+const user = {
+    organization: "MRM LTD.",
+    firstName: "Rafiq",
+    lastName: "Mia",
+    isMarried: true,
+};
+user.organization = "Software LTD.";
+console.log(user);
+console.log(user);
 //# sourceMappingURL=nonPrimitive.js.map
---
+++ src/nonPrimitive.ts
@@ -0,0 +1,38 @@
+// array , object
+
+// ts - tuple
+
+let bazarList: string[] = ["eggs", "milk", "sugar"];
+
+bazarList.push(true);
+
+let mixedArr: (string | number)[] = ["eggs", 12, "milk", 1, "sugar", 2];
+
+mixedArr.push(true);
+
+let coordinates: [number, number] = [20, 30, 50];
+
+let couple: [string, string] = ["Husband", "wife"];
+
+let destination: [string, string, number] = ["Dhaka", "Chattogram", 3];
+
+// reference type : object
+
+const user: {
+  readonly organization: string; // access modifier
+  firstName: string;
+  middleName?: string; // optional type
+  lastName: string;
+  isMarried: boolean;
+} = {
+  organization: "MRM LTD.",
+  firstName: "Rafiq",
+  lastName: "Mia",
+  isMarried: true,
+};
+
+user.organization = "Software LTD.";
+
+console.log(user);
+
+console.log(user);
