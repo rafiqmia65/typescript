e48f0c661f18639f09f2f1a42eba8feca305cced
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-11-17 14:39:22 +0600

    Class

---
+++ src/OopTypescript/class.ts
@@ -0,0 +1,37 @@
+//oop - class >>> object
+
+// class Animal {
+//   name: string;
+//   species: string;
+//   sound: string;
+
+//   constructor(name: string, species: string, sound: string) {
+//     this.name = name;
+//     this.species = species;
+//     this.sound = sound;
+//   }
+
+//   makeSound() {
+//     console.log(`${this.name} is making  sound: ${this.sound}`);
+//   }
+// }
+
+
+//parameter properties
+
+class Animal {
+  constructor(public name: string, public species: string, public sound: string) {
+   
+  }
+
+  makeSound() {
+    console.log(`${this.name} is making  sound: ${this.sound}`);
+  }
+}
+
+
+const dog = new Animal("dogesh bhai", "dog", "Ghew ghew");
+
+// const cat = new Animal("Cat bhai", "Cat", "mewa meaw");
+
+dog.makeSound;
